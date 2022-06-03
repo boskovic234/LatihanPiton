8f25fc374a6d204022ba4f0dd651af03465143e6
Isman Kusuma <boskovic234@gmail.com>
2022-06-03 23:37:27 +0700

    Rubah 1

---
+++ .ipynb_checkpoints/LatihanPiton/Coba.py
@@ -0,0 +1,9 @@
+import statistics as stat
+
+nilaimean = stat.mean([1,2,3,4,5,6,7,8])
+nilaimedian = stat.median([2,3,4,5,6,7,8,9,5,4])
+
+print(nilaimean)
+print(nilaimedian)
+
+#COBA CEK asjhdajkshdjkashdjkashdjkashkj
