b3b83997b50306cff00e9ed380f5ad9df8d1c4bc
Isman Kusuma <boskovic234@gmail.com>
2022-06-04 00:05:49 +0700

    Rubah 3

--- .ipynb_checkpoints/LatihanPiton/Coba.py
+++ .ipynb_checkpoints/LatihanPiton/Coba.py
@@ -5,10 +5,11 @@ nilaimedian = stat.median([2,3,4,5,6,7,8,9,5,4])
 
 print(nilaimean)
 print(nilaimedian)
-print(nilaimedian)
-print(nilaimedian)
-print(nilaimedian)
-print(nilaimedian)
-print(nilaimedian)
 
-#COBA CEK asjhdajkshdjkashdjkashdjkashkjvfgddfasdasdsadd
+print(123123123123+1)
+
+print(10)
+print(type('10'))
+
+a = ['foo','kucing','tikus']
+print (a)
