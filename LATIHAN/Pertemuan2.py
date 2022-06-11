fae5a029e058d7c7e4f92c5bb575bad823c4ab7b
Isman Kusuma <boskovic234@gmail.com>
2022-06-11 16:25:13 +0700

    20220611b

--- LATIHAN/Pertemuan2.py
+++ LATIHAN/Pertemuan2.py
@@ -49,4 +49,35 @@ import numpy as np
 # jumlahHewan()
 # jumlahKelinci()
 
-print (np.version.version)
+
+# ---------------------------
+# print (np.version.version)
+
+# a = np.array([1,2,3,4,5])
+# a = np.append(a, [6,7])
+# print (a)
+
+# a = np.delete(a, 1)
+# print (a)
+
+
+# b = np.zeros(5)
+# print (b)
+
+# -------------------------
+# data = np.array([1,2,3,4,5])
+
+# print(data)
+# print(data[0])
+# print(data[1])
+# print(data[0:3])
+# print(data[1:]) #mengambil data seluruhnya setelah index/array tsb
+# print(data[-2:])
+
+# ----------------------------------
+arr = np.array([[1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12],
+                     [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]])
+
+arr = np.hsplit(arr, 2)
+
+print (arr)
