3e36172cd850dff9edb72719519237c2a4028646
Isman Kusuma <boskovic234@gmail.com>
2022-06-24 00:15:45 +0700

    20220623

--- PythonProject/ProjectIsman.py
+++ PythonProject/ProjectIsman.py
@@ -1,7 +1,15 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from tabulate import tabulate
 
-nycdata = pd.read_csv("nyc-rolling-sales.csv")
+# Untuk load data dari CSV
+police = pd.read_csv("police_deaths_in_america.csv")
+
+# Untuk manipulasi tampilan
+pd.set_option("display.max.rows", None)
+pd.set_option("display.max.column", None)
+# pd.set_option("display.precision", 2)
 
 # Untuk cek data berhasil di load
 # print(nycdata.head(10))
@@ -9,13 +17,61 @@ nycdata = pd.read_csv("nyc-rolling-sales.csv")
 # untuk compile 
 # python -m py_compile namafile.py
 
-# print(nycdata.info())
-# print(nycdata.describe())
+print(police.info())
+# print(nycdata)
+
+# tahun = input("Masukkan tahun : ")
 
 
-## -------------------------------BATAS
+
+
+
+# meandata = police[
+#         ['Rank',
+#         'Year',
+#         'State']
+#         ]
+
+# # print(meandata)
+# print("-------------------------------")
+# # print(meandata["Rank"].value_counts())
+# print("-------------------------------")
+# patrolmandata = meandata.loc[
+#         (meandata["Rank"] == "Patrolman") &
+#          (meandata["State"] == "New York") &
+#          (meandata["Year"] > int(tahun))].value_counts(["Rank","State","Year"]).reset_index(name="Count")
+# print("-------------------------------")
+# print(patrolmandata)         
+# df.loc[df["fran_id"] == "Lakers", "team_id"].value_counts()
+
+# print(meandata.describe())
+
+# datarumah = nycdata.loc[(nycdata[""])]
+
+
+# print(tabulate(nycdata, headers='keys', tablefmt='psql'))
+
+
+# Munculin Grafik
+# data = {'Data1':[1,2,3,4,5],'Data2':[1000,2000,3000,4000,5000]}
+
+# df = pd.DataFrame(data,columns=['Data1','Data2'])
+# print(df)
+
+# patrolmandata.plot(x ='Year', y='Count', kind='scatter')
+# plt.show()
+
+## -------------------------------BATAS-----------------------
 def mean():
     print("Memanggil Function Mean")
+
+    data = {'Data1':[1,2,3,4,5],'Data2':[1000,2000,3000,4000,5000]}
+
+    df = pd.DataFrame(data,columns=['Data1','Data2'])
+    print(df)
+
+    df.plot(x ='Data1', y='Data2', kind='scatter')
+    plt.show()
 
 def median():
     print("Memanggil Function Median")    
@@ -33,7 +89,37 @@ def std():
     print("Memanggil Function Standard Deviation")
 
 def prob():
-    print("Memanggil Function Probability Distribution")        
+    print("Memanggil Function Probability Distribution")       
+
+def deadyear():
+        matitahun = police[["Year"]].value_counts(["Year"]).reset_index(name='Count')
+        
+        # matitahundf = pd.DataFrame(matitahun, columns=["Years"])
+        print(matitahun)
+        matitahun.plot(x = 'Year', y='Count', kind='scatter')
+        plt.show()
+        # print(matitahun["Year"].value_counts(["Year"]).reset_index(name="Count"))
+        # matitahun["Year"].value_counts()
+
+def deadstate():
+        matistate = police[["State"]].value_counts(["State"]).reset_index(name='Count')
+        print(matistate)
+        matistate.plot(x = 'State', y='Count', kind='scatter')
+        plt.show()
+
+def deadreason():
+        matireason = police[["Cause_of_Death"]].value_counts(["Cause_of_Death"]).reset_index(name='Count')
+        # matireason = matireason.loc[(matireason["Cause_of_Death"] != "Gunfire")]
+        print(matireason)
+        # matireason.plot(x = 'CauseOfDeath', y='Count', kind='scatter')
+        # plt.show()
+
+def deaddept():
+        matidept = police[["Department"]].value_counts(["Department"]).reset_index(name='Count')
+        print(matidept)
+        # matidept.plot(x='Department', y='Count', kind='scatter')
+        # plt.show()
+
 
 print(" ")
 namaanda = input("Masukkan nama anda : ")
@@ -41,13 +127,13 @@ namaanda = input("Masukkan nama anda : ")
 while True:
     
     print(" ")
-    print("Selamat datang di pusat informasi property New York, "+namaanda.title())
+    print("Selamat datang di pusat informasi Kepolisian Amerika Serikat, "+namaanda.title())
     print(" ")
     print("Informasi apa yang anda butuhkan, silahkan pilih salah satu :")
-    print("1. Mean Data Information")
-    print("2. Median Data Information")
-    print("3. Modus Data Information")
-    print("4. Range Data Information")
+    print("1. Jumlah kematian per tahun")
+    print("2. Jumlah kematian per daerah")
+    print("3. Jumlah penyebab kematian")
+    print("4. Jumlah kematian per departemen")
     print("5. Variance Data Information")
     print("6. Standard Deviation Data Information")
     print("7. Probability Distribution Data Information")
@@ -55,32 +141,36 @@ while True:
     pilihan = input("Pilihan anda no : ")
 
     if pilihan.lower() == "1":
-        print("Pilihan 1")
-        mean()
+        print("Informasi jumlah kematian per tahun : ")
+        # mean()
+        deadyear()
         print(" ")
         ulang = input("Apakah anda ingin kembali ke menu utama ? (Ya/Tidak) : ")
         if ulang.lower() == "tidak":
             break
 
     if pilihan.lower() == "2":
-        print("Pilihan 2")
-        median()
+        print("Informasi jumlah kematian per daerah")
+        # median()
+        deadstate()
         print(" ")
         ulang = input("Apakah anda ingin kembali ke menu utama ? (Ya/Tidak) : ")
         if ulang.lower() == "tidak":
             break
 
     if pilihan.lower() == "3":
-        print("Pilihan 3")
-        modus()
+        print("Informasi jumlah penyebab kematian")
+        # modus()
+        deadreason()
         print(" ")
         ulang = input("Apakah anda ingin kembali ke menu utama ? (Ya/Tidak) : ")
         if ulang.lower() == "tidak":
             break
 
     if pilihan.lower() == "4":
-        print("Pilihan 4")
-        range()
+        print("Informasi jumlah kematian per departement")
+        # range()
+        deaddept()
         print(" ")
         ulang = input("Apakah anda ingin kembali ke menu utama ? (Ya/Tidak) : ")
         if ulang.lower() == "tidak":
